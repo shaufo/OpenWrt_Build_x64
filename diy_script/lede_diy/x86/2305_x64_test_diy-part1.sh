a512d49661d315420b8e0818e148cdbdff697f6c
gxnas <52808122+gxnas@users.noreply.github.com>
2024-11-22 22:21:47 +0800

    Update 2305_x64_test_diy-part1.sh

--- diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
+++ diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
@@ -9,6 +9,7 @@
 # Add a feed source
 sed -i "/helloworld/d" feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
 
 mkdir wget
 
