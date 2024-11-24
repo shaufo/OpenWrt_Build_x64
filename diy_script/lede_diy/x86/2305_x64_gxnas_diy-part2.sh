05908ec02ffbb9b1ad1b33f2950d2033b6870fdf
shaufo <63514384+shaufo@users.noreply.github.com>
2024-11-24 22:06:11 +0800

    Update 2305_x64_gxnas_diy-part2.sh
    修改ip

--- diy_script/lede_diy/x86/2305_x64_gxnas_diy-part2.sh
+++ diy_script/lede_diy/x86/2305_x64_gxnas_diy-part2.sh
@@ -16,12 +16,12 @@ source $GITHUB_WORKSPACE/diy_script/function.sh
 rm -rf package/custom; mkdir package/custom
 
 # 修改主机名字，修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-GXNAS'" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/hostname='.*'/hostname='OpenWrt-GXNAS'/g" ./package/base-files/files/bin/config_generate
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='Pangluyou'" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/hostname='.*'/hostname='Pangluyou'/g" ./package/base-files/files/bin/config_generate
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/luci2/bin/config_generate
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./d' package/lean/default-settings/files/zzz-default-settings
