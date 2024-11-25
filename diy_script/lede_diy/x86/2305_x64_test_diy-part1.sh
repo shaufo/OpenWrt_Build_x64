4dc2aabdb6b7b17e7cda76860a7c64b85418cc20
gxnas <52808122+gxnas@users.noreply.github.com>
2024-11-25 10:54:36 +0800

    Update 2305_x64_test_diy-part1.sh

--- diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
+++ diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
@@ -10,7 +10,6 @@
 sed -i "/helloworld/d" feeds.conf.default
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git Jejz168 https://github.com/Jejz168/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git wjz304 https://github.com/wjz304/openwrt-packages;lede' feeds.conf.default
 
 mkdir wget
 
@@ -33,7 +32,7 @@ ver510=`grep "LINUX_VERSION-5.10 ="  include/kernel-5.10 | cut -d . -f 3`
 ver515=`grep "LINUX_VERSION-5.15 ="  include/kernel-5.15 | cut -d . -f 3`
 ver61=`grep "LINUX_VERSION-6.1 ="  include/kernel-6.1 | cut -d . -f 3`
 ver66=`grep "LINUX_VERSION-6.6 ="  include/kernel-6.6 | cut -d . -f 3`
-ver612=`grep "LINUX_VERSION-6.12 ="  include/kernel-6.12 | cut -d . -f 3`
+#ver612=`grep "LINUX_VERSION-6.12 ="  include/kernel-6.12 | cut -d . -f 3`
 if [ "$str1" = "5.4" ];then
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_bios.img.gz
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver54}_uefi.img.gz
@@ -59,14 +58,14 @@ elif [ "$str1" = "6.6" ];then
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver66}_bios.img.gz
    mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver66}_uefi.img.gz
    fi
-elif [ "$str1" = "6.12" ];then
-   if [ ! $ver612 ]; then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}0_bios.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}0_uefi.img.gz
-  else
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_bios.img.gz
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_uefi.img.gz
-   fi
+#elif [ "$str1" = "6.12" ];then
+   #if [ ! $ver612 ]; then
+   #mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}0_bios.img.gz
+   #mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}0_uefi.img.gz
+  #else
+   #mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_bios.img.gz
+   #mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64_${str1}.${ver612}_uefi.img.gz
+   #fi
 fi
 #md5
 ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_dev_md5
