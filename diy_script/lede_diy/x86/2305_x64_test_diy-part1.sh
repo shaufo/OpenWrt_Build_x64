499bfc6965b7bc09c6465c391549f3caf26b4ccb
gxnas <52808122+gxnas@users.noreply.github.com>
2024-12-03 08:51:49 +0800

    Update 2305_x64_test_diy-part1.sh

--- diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
+++ diy_script/lede_diy/x86/2305_x64_test_diy-part1.sh
@@ -9,7 +9,7 @@
 # Add a feed source
 sed -i "/helloworld/d" feeds.conf.default
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
-sed -i '$a src-git gxnas https://github.com/gxnas/OpenWrt-Packages' feeds.conf.default
+sed -i '$a src-git fichenx https://github.com/fichenx/openwrt-package' feeds.conf.default
 
 mkdir wget
 
